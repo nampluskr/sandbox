dfc0d222b7275c643ae422804478c161aa3bf9af
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-03-16 18:43:33 +0900

    Create 11_dfm.py

---
+++ defectvad_rev/20260316/11_dfm.py
@@ -0,0 +1,90 @@
+import os
+import sys
+
+SOURCE_DIR = "/home/namu/myspace/NAMU/defectvad_rev/src"
+if SOURCE_DIR not in sys.path:
+    sys.path.insert(0, SOURCE_DIR)
+
+os.environ["BACKBONE_DIR"] = "/home/namu/myspace/NAMU/backbones"
+
+#####################################################################
+# Model test
+#####################################################################
+import logging
+import torch
+
+from defectvad.models.dfm.torch_model import DFMModel
+from trainer import BaseTrainer
+
+
+class DFMTrainer(BaseTrainer):
+    def __init__(self, model, loss_fn=None, device=None, evaluator=None):
+        super().__init__(model, loss_fn, device, evaluator)
+
+    def on_train_start(self):
+        super().on_train_start()
+        self.max_epochs = 1
+
+    def training_step(self, batch):
+        images = batch["image"].to(self.device)
+        _ = self.model(images)
+        return {"loss": torch.tensor(0.0).float().to(self.device)}
+
+    def on_train_end(self):
+        super().on_train_end()
+        self.model.fit()
+
+
+if __name__ == "__main__":
+
+    from mvtec import get_dataloader
+    from evaluator import Evaluator
+    from utils import set_seed, set_logging
+
+    EXPERIMENT_NAME = "mvtec_11_dfm"
+    LOG_FILE = f"{EXPERIMENT_NAME}.log"
+
+    set_seed(42)
+    output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "outputs"))
+    set_logging(output_dir, LOG_FILE)
+    logger = logging.getLogger(__name__)
+    logger.info(f" > Logging initialized: {LOG_FILE}")
+
+    train_loader = get_dataloader(
+        split="train", 
+        data_dir="/home/namu/myspace/NAMU/datasets/mvtec",
+        category=["carpet", "grid", "leather", "tile", "wood"],
+        # category=["tile"],
+        batch_size=16,
+        img_size=256,
+        crop_size=224,
+        normalize=True,
+    )
+    test_loader = get_dataloader(
+        split="test", 
+        data_dir="/home/namu/myspace/NAMU/datasets/mvtec",
+        category=["carpet", "grid", "leather", "tile", "wood"],
+        # category=["tile"],
+        batch_size=1,
+        img_size=256,
+        crop_size=224,
+        normalize=True,
+    )
+
+    # resnet18 / resnet50
+    model = DFMModel(
+            backbone="resnet50",
+            layer="layer3", 
+            pooling_kernel_size=4, 
+            n_comps=0.97,
+            score_type="fre",
+        )
+    trainer = DFMTrainer(model)
+    trainer.fit(train_loader, max_epochs=1)
+
+    evaluator = Evaluator(model)
+    image_results = evaluator.evaluate_image_level(test_loader)
+    logger.info("Image-level: " + ", ".join([f"{k}:{v:.3f}" for k, v in image_results.items()]))
+
+    del train_loader
+    del test_loader
