79eabf908d77fd749d29be4b5f413bbb0d335980
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-03-17 15:38:41 +0900

    Create 12_dfkde.py

---
+++ defectvad_rev/20260317/torch_trainer/12_dfkde.py
@@ -0,0 +1,24 @@
+import torch
+
+from defectvad.common.trainer import BaseTrainer
+from defectvad.models.dfkde.torch_model import DfkdeModel
+
+
+class DfkdeTrainer(BaseTrainer):
+    def __init__(self, model, loss_fn=None, device=None, evaluator=None):
+        if not isinstance(model, DfkdeModel):
+            raise TypeError(f"Unexpected  model: {type(model).__name__}")
+        super().__init__(model, loss_fn, device, evaluator)
+
+    def on_train_start(self):
+        super().on_train_start()
+        self.max_epochs = 1
+
+    def training_step(self, batch):
+        images = batch["image"].to(self.device)
+        _ = self.model(images)
+        return {"loss": torch.tensor(0.0).float().to(self.device)}
+
+    def on_train_end(self):
+        super().on_train_end()
+        self.model.fit()
